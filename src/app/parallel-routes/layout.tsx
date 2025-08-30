ad7e5cbc7c701ba5754a4b3584d2875494034e3f
lizuncong <2900170896@qq.com>
2025-08-30 22:39:11 +0800

    fix: stas11

--- src/app/parallel-routes/layout.tsx
+++ src/app/parallel-routes/layout.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Follow lzc studying Next.js',
-  description: 'te1st',
+  description: '11',
 };
 
 export default function RootLayout({
